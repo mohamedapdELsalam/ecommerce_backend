c9a2b6ad6c25ddafa34f99be4b89a3ccc1405e61
mohamedapdELsalam <mohamed@gmail.com>
2025-06-13 02:21:16 +0300

    add seleccted variants in add to cart endpoint

--- users/cart/add.php
+++ users/cart/add.php
@@ -4,9 +4,11 @@ include "../../connect.php";
 
 $itemId = filterRequest("itemId");
 $userId = filterRequest("userId");
+$variantId = filterRequest("variantId");
 
-$stmt = $con->prepare("INSERT INTO `cart` (`cart_itemid`, `cart_userid`) VALUES (?,?)");
-$stmt->execute(array($itemId , $userId));
+
+$stmt = $con->prepare("INSERT INTO `cart` (`cart_itemid`, `cart_userid`,`cart_selected_variant`) VALUES (?,?,?)");
+$stmt->execute(array($itemId , $userId,$variantId));
 $count = $stmt->rowCount();
 
 $stmt = $con->prepare("SELECT COUNT(cart.cart_id)  FROM `cart` WHERE cart_itemid = ? AND cart_userid =? AND cart_orders = 0");
